7793d25e379cdb2fe74854b9296c2a0838c6294b
Snehal <snehalabhale01@gmail.com>
2023-10-11 11:41:18 -0700

    Phase2 Mock Challenge submission

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants]= useState([]);
+  const [search, setSearch]= useState("");
+
+  useEffect(()=> {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(data => setPlants(data))
+  }, [])
+
+  function onhandleFormSubmit(newPlantObj)
+  {
+    fetch("http://localhost:6001/plants",{
+      method: "POST",
+      headers:{
+      "Content-Type": "application/json"
+    },
+      body: JSON.stringify(newPlantObj)
+    })
+    .then(response => response.json())
+    .then(newPlant=> setPlants([...plants, newPlant]))
+  }
+  const filteredPlants= plants.filter(plant=> plant.name.toLowerCase().includes(search.toLowerCase()));
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onhandleFormSubmit={onhandleFormSubmit}/>
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
